7e35f2c30dcc11d9b908b8dd3d7bacb8c8960995
“marenah7git config --global user.name “marenah7 <muhammadmarenah@Muhammads-MBP.attlocal.net>
2023-04-02 21:42:33 -0400

    Add 01-read_inputcopy.sh file

---
+++ 01-read_inputcopy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, Muhammad Marenah"
+
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
