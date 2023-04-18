328bb0e37ef495dac1824c356ecd7612ac6865e2
“marenah7git config --global user.name “marenah7 <muhammadmarenah@Muhammads-MBP.attlocal.net>
2023-04-17 23:47:46 -0400

    Added Controller.java

---
+++ Desktop/Controller.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Controller {
+
+    //Muhammad Marenah
+    private Scanner scanner;
+
+    public Controller() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String[] getUserInput() {
+        String input = scanner.nextLine();
+        return input.split(" ");
+    }
+}
+
+
